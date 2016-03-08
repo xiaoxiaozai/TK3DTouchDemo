709e50f2401d7f97722121d91a479cdc29ee8a30
xiaoxiaozai <foreverlosda@vip.qq.com>
2016-03-08 14:27:46 +0800

    update

---
+++ TK3DTouchDemoTests/TK3DTouchDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  TK3DTouchDemoTests.m
+//  TK3DTouchDemoTests
+//
+//  Created by 翟宇 on 16/3/8.
+//  Copyright © 2016年 翟宇. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface TK3DTouchDemoTests : XCTestCase
+
+@end
+
+@implementation TK3DTouchDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
